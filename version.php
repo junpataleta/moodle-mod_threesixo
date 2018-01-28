aad598d6603d7d9e4da9551ad77412c8cfec6aa1
Jun Pataleta <jun@moodle.com>
2018-01-28 09:44:27 +0800

    Bump version to 3.4-r4.

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017081602;        // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2017081603;        // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2017110800.00;     // Requires this Moodle version.
 $plugin->component = 'mod_threesixo';   // Full name of the plugin (used for diagnostics).
 $plugin->cron      = 0;
-$plugin->release = 'v3.4-r3';
+$plugin->release = 'v3.4-r4';
 $plugin->maturity = MATURITY_STABLE;
