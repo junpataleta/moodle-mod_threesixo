b7eed4808dc1183d48a33bead4e6ae5c69629ce0
Jun Pataleta <jun@moodle.com>
2018-05-26 01:41:25 +0800

    Bump version for 3.6 development

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018012800;        // The current module version (Date: YYYYMMDDXX)
-$plugin->requires  = 2017110800.00;     // Requires this Moodle version.
+$plugin->version   = 2018052600;        // The current module version (Date: YYYYMMDDXX)
+$plugin->requires  = 2018051700.00;     // Requires Moodle 3.6+.
 $plugin->component = 'mod_threesixo';   // Full name of the plugin (used for diagnostics).
 $plugin->cron      = 0;
-$plugin->release = 'v3.5-r4';
+$plugin->release = 'v36dev';
 $plugin->maturity = MATURITY_ALPHA;
