d4f4562a4b39d7a986c0efcb5b6f3fd5752e1484
Jun Pataleta <jun@moodle.com>
2020-01-14 22:42:25 +0800

    Bump version to v37.1.0

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2019052000;        // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2019052001;        // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2019052000.00;     // Requires Moodle 3.7+.
 $plugin->component = 'mod_threesixo';   // Full name of the plugin (used for diagnostics).
 $plugin->cron      = 0;
-$plugin->release = 'v37.0.0';
+$plugin->release = 'v37.1.0';
 $plugin->maturity = MATURITY_STABLE;
