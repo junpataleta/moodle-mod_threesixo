cb30af181d4537fb16d1d9687735a9a97f08a73d
Jun Pataleta <jun@moodle.com>
2021-06-14 10:40:44 +0800

    Bump to version 3.10.0
    With the Moodle branch numbering scheme changed since 3.10, having a
    3-digit version number feels weird. So I'm changing the versioning
    scheme to follow the actual Moodle version supported instead of basing
    it on the Moodle branch number.

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2019111800;        // The current module version (Date: YYYYMMDDXX)
-$plugin->requires  = 2019111800.00;     // Requires Moodle 3.8+.
+$plugin->version   = 2021061400;        // The current module version (Date: YYYYMMDDXX).
+$plugin->requires  = 2020110900.00;     // Requires Moodle 3.10+.
 $plugin->component = 'mod_threesixo';   // Full name of the plugin (used for diagnostics).
 $plugin->cron      = 0;
-$plugin->release = 'v38.0.0';
+$plugin->release = 'v3.10.0';
 $plugin->maturity = MATURITY_STABLE;
