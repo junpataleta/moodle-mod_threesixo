7b4c9fe9bfc436d044a884e166e55c7b4c3dbe95
Jun Pataleta <jun@moodle.com>
2026-06-20 19:43:07 +0800

    Bump to v5.1.0

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025100700;        // The current module version (Date: YYYYMMDDXX).
-$plugin->requires  = 2025041400.00;     // Requires Moodle 5.0+.
+$plugin->version   = 2026062000;        // The current module version (Date: YYYYMMDDXX).
+$plugin->requires  = 2025100600.00;     // Requires Moodle 5.1+.
 $plugin->component = 'mod_threesixo';   // Full name of the plugin (used for diagnostics).
 $plugin->cron      = 0;
-$plugin->release = 'v5.0.0';
+$plugin->release = 'v5.1.0';
 $plugin->maturity = MATURITY_STABLE;
